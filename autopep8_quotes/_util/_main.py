0733281986bb6a006f67361a9cf36a80158ab3df
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-04-17 23:52:03 +0600

    Change behavior
    Before: All modules run on one token
    After: Module run on all tokens in file, then next module. It's better to use one-run script with check-hard on the end of run.

--- autopep8_quotes/_util/_main.py
+++ autopep8_quotes/_util/_main.py
@@ -22,43 +22,46 @@ def format_file(args: SimpleNamespace) -> Any:
     args._read_encoding = detect_encoding(args._read_filename)
     with open_with_encoding(args._read_filename, encoding=args._read_encoding, mode="rb") as input_file:
         source = input_file.read()
-        source = source.decode(args._read_encoding)
-        formatted_source = format_code(
-            source,
-            args=args,
-            filename=args._read_filename)
-
-    if source != formatted_source:
-        result: List[Any] = [False]
-        for _dict_mod in args._start_save_order:
-            func = _dict_mod["module"].formatter().show_or_save
-            res = func(args, source, formatted_source, **_dict_mod["kwargs"])
-            if res is None:
-                pass
-            elif isinstance(res, (list, tuple)):
-                if res[0].lower() == "return":
-                    if len(res[1:]) == 1:
-                        result.append(res[1])
-                    else:
-                        result.append(res[1:])
-            elif isinstance(res, str):
-                if res.lower() == "continue":
-                    continue
-
-        if any(result):
-            return 1
+        formatted_source = source.decode(args._read_encoding)
+        for _dict_mod in args._start_parse_order:
+            # Update source every run
+            source = formatted_source
+            formatted_source = format_code(
+                source,
+                args=args,
+                filename=args._read_filename,
+                fmt_classs=_dict_mod["module"].formatter())
+    
+            if source != formatted_source:
+                result: List[Any] = [False]
+                func = _dict_mod["module"].formatter().show_or_save
+                res = func(args, source, formatted_source, **_dict_mod["kwargs"])
+                if res is None:
+                    pass
+                elif isinstance(res, (list, tuple)):
+                    if res[0].lower() == "return":
+                        if len(res[1:]) == 1:
+                            result.append(res[1])
+                        else:
+                            result.append(res[1:])
+                elif isinstance(res, str):
+                    if res.lower() == "continue":
+                        continue
+        
+                if any(result):
+                    return 1
     return 0
 
 
-def format_code(source: str, args: SimpleNamespace, filename: str) -> Any:
+def format_code(source: str, args: SimpleNamespace, filename: str, fmt_classs: Any) -> Any:
     """Return source code with quotes unified."""
     try:
-        return _format_code(source, args, filename)
+        return _format_code(source, args, filename, fmt_classs=fmt_classs)
     except (tokenize.TokenError, IndentationError):  # pragma: no cover
         return source
 
 
-def _format_code(source: str, args: SimpleNamespace, filename: str) -> Any:
+def _format_code(source: str, args: SimpleNamespace, filename: str, fmt_classs: Any) -> Any:
     """Return source code with quotes unified."""
     if not source:
         return source
@@ -67,20 +70,15 @@ def _format_code(source: str, args: SimpleNamespace, filename: str) -> Any:
 
     sio = io.StringIO(source)
     save_list = []
-    for (token_type,
-         token_string,
-         start,
-         end,
-         line) in tokenize.generate_tokens(sio.readline):
+
+    for (token_type, token_string, start, end, line) in tokenize.generate_tokens(sio.readline):
         if token_type == tokenize.STRING:
             token_dict = get_token_dict(token_type, token_string, start, end, line, filename)
 
             if args.save_values_to_file:
                 save_list.append(token_dict)
 
-            for _dict_mod in args._start_parse_order:
-                func = _dict_mod["module"].formatter().parse
-                token_string = func(token_string, args=args, token_dict=token_dict, **_dict_mod["kwargs"])
+                token_string = fmt_classs.parse(token_string, args=args, token_dict=token_dict, **_dict_mod["kwargs"])
 
         modified_tokens.append((token_type, token_string, start, end, line))
 
