d703c64550b34e7c489468ef681281eced1705ee
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-05-01 19:23:59 +0600

    tests

---
+++ tests/good/test.py
@@ -0,0 +1,21 @@
+a = '''"Some text"'''
+b = '''f"Some text"'''
+
+
+entry_points_1={
+        'autopep8_quotes.formatter': [ # noqa
+            "lowercase_string_prefix = autopep8_quotes.modules.formater.lowercase_string_prefix:formatter",
+    ],
+}
+
+entry_points_2={
+        'autopep8_quotes.formatter': [ # another comment
+            "lowercase_string_prefix = autopep8_quotes.modules.formater.lowercase_string_prefix:formatter",
+    ],
+}
+
+entry_points_2={
+        'autopep8_quotes.formatter': [
+            "lowercase_string_prefix = autopep8_quotes.modules.formater.lowercase_string_prefix:formatter",
+    ],
+}
