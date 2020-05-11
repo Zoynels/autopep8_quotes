27e6a1794b27f899bf2aaeb52c9b531a97f0d2fb
Zoynels <5841616+Zoynels@users.noreply.github.com>
2020-05-11 23:32:24 +0600

    Fix

--- tox_windows.bat
+++
@@ -1,8 +0,0 @@
-::tox
-::tox -e check, mypy
-::tox -e pep8
-::tox -e check_manifest
-tox -e autopep8_quotes
-
-pause
-tox_windows.bat
--- upload_codecov.bat
+++
@@ -1,7 +0,0 @@
-codecov.exe --required --verbose -f coverage.xml -t %CODECOV_TOKEN__autopep8_quotes%
-
-if errorlevel 1 (
-    pause
-) ELSE (
-    exit
-)
